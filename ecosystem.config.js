e1e8a58b58b24c3f52cdc4df0c60e60667d72af9
Siddartha83795 <alonrfox3797@gmail.com>
2025-12-07 18:21:39 +0530

    feat: update deployment config to match user preference

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-    apps: [
-        {
-            name: "dinehub",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production",
-                PORT: 3000,
-            },
+    apps: [{
+        name: "dinehub",
+        script: "npm",
+        args: "start",
+        instances: 1,
+        exec_mode: "fork",
+        env_dev: {
+            NODE_ENV: "development",
+            PORT: 3000
         },
-    ],
+        env_prod: {
+            NODE_ENV: "production",
+            PORT: 3000
+        }
+    }]
 };
